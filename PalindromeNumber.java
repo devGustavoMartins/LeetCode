7862b8216dc3275e6fddb97593e5d5c9dfd23649
Gustavo <gustavo.om1206@gmail.com>
2024-05-03 19:19:01 -0300

    Palindrome Number solution

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,2 +1,13 @@
 public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String str1 = String.valueOf(x);
+        String str2 = new StringBuilder(str1).reverse().toString();
+        return str1.equals(str2);
+    }
 }
